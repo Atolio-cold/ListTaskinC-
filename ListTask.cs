603495fee2c8df734fc6551e649a1d016cf41d0f
Antoni <84175644+Atolio-cold@users.noreply.github.com>
2024-11-15 22:41:42 +0100

    Add files via upload

--- ListTask.cs
+++ ListTask.cs
@@ -1,33 +1,38 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 class ListTask
 {
-
     public static void Main()
     {
-
+        // Tworzymy listę, która będzie przechowywać unikalne miasta
         List<string> list = new List<string>();
 
-        Console.WriteLine("Podaj nazwę miasta #: ");
+        Console.WriteLine("Podaj nazwę 3 unikalnych miast:");
 
-        for (int i = 0; i < 3; i++)
+        for (int i = 0; i < 3; i++) // Pętla działa dokładnie 3 razy
         {
-            string element = Console.ReadLine();
-            list.Add(element);
+            Console.Write($"Podaj nazwę miasta #{i + 1}: ");
+            string city = Console.ReadLine();
 
+            // Sprawdzamy, czy miasto już jest w liście
+            if (list.Contains(city))
+            {
+                Console.WriteLine("To miasto już istnieje. Wprowadź inne.");
+                i--; // Cofamy licznik, by użytkownik mógł ponownie wprowadzić wartość
+            }
+            else
+            {
+                // Dodajemy unikalne miasto do listy
+                list.Add(city);
+            }
         }
 
-        Console.WriteLine("Podaj miasta");
-
-        foreach (var element in list)
+        // Wyświetlamy wszystkie dodane miasta
+        Console.WriteLine("\nWszystkie miasta:");
+        foreach (var city in list)
         {
-            Console.WriteLine($"Cities: {element}");
+            Console.WriteLine($"Miasto: {city}");
         }
-
-
     }
-
 }
